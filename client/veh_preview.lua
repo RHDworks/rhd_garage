46e2426b2d4474ad8a49c45bacc54d346664dfd0
Reyghita Hafizh <reyhafizh07@gmail.com>
2025-05-05 10:00:52 +0700

    Update veh_preview.lua

--- client/veh_preview.lua
+++ client/veh_preview.lua
@@ -80,17 +80,20 @@ function vehiclePreview:getStats()
     if not self.vehEntity or not DoesEntityExist(self.vehEntity) then
         return
     end
-    
+
     local fInitialDriveMaxFlatVel = GetVehicleHandlingFloat(self.vehEntity, "CHandlingData", "fInitialDriveMaxFlatVel")
     local fInitialDriveForce = GetVehicleHandlingFloat(self.vehEntity, "CHandlingData", "fInitialDriveForce")
     local fBrakeForce = GetVehicleHandlingFloat(self.vehEntity, "CHandlingData", "fBrakeForce")
     local fTractionCurveMax = GetVehicleHandlingFloat(self.vehEntity, "CHandlingData", "fTractionCurveMax")
     local fTractionCurveMin = GetVehicleHandlingFloat(self.vehEntity, "CHandlingData", "fTractionCurveMin")
+    local fSteeringLock = GetVehicleHandlingFloat(self.vehEntity, "CHandlingData", "fSteeringLock")
 
-    stats.speed = math.floor((fInitialDriveMaxFlatVel / 50.0) * 100)
+    local maxSpeedKMH = (fInitialDriveMaxFlatVel * 0.75) * 3.6
+
+    stats.speed = math.floor((maxSpeedKMH / 530.0) * 100)
     stats.acceleration = math.floor((fInitialDriveForce / 1.3) * 100)
     stats.braking = math.floor((fBrakeForce / 2.0) * 100)
-    stats.handling = math.floor(((fTractionCurveMax + fTractionCurveMin) / 4.0) * 100)
+    stats.handling = math.floor(((fSteeringLock / 42.0) * 0.7 + ((fTractionCurveMax + fTractionCurveMin) / 5.5) * 0.3) * 100)
     stats.traction = math.floor((fTractionCurveMax / 3.0) * 100)
 
     for k, v in pairs(stats) do
